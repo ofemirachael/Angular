13a6f4109784f032c92030bffc14d79a2cf2241d
Olayode Oluwafemi Rachael <rachael.f.olayode@gmail.com>
2024-03-25 17:50:36 -0600

    begininng my angular journey

---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warningalert',
+  templateUrl: './warningalert.component.html',
+  styleUrl: './warningalert.component.css',
+})
+export class WarningalertComponent {}
